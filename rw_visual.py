cf699cbafc68a89eec38180020f94736dd78358d
Bill Cathey <billcathey@hotmail.com>
2023-02-24 15:12:59 -0500

    Wrap script in while loop to generate new walks without rerunning program

--- rw_visual.py
+++ rw_visual.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks, as long as program is active.
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
 
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
 
-plt.show()
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
