004d3d148d4339407a477eab14d09557c8cdfd57
Bill Cathey <billcathey@hotmail.com>
2023-02-24 15:24:44 -0500

    Style random walk plot, add larger number of plot points

--- rw_visual.py
+++ rw_visual.py
@@ -4,13 +4,21 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    fig, ax = plt.subplots(figsize=(16, 9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+    # Emphasize first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
