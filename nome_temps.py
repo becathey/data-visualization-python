61fc0f3fd3ab6d5654cdf6f4ae471867fa42c161
Bill Cathey <billcathey@hotmail.com>
2023-02-25 21:48:28 -0500

    Extract data and visualize high and low temperatures

--- nome_temps.py
+++ nome_temps.py
@@ -7,21 +7,25 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    # Get dates and high temps from this file
-    dates, highs = [], []
+    # Get dates, high and low temps from this file
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[6])
+        high = int(row[7])
+        low = int(row[8])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
-# Plot high temperatures
+# Plot high and low temperatures
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.3)
 
 # Format plot
-plt.title('Daily high temperatures, 2022', fontsize=24)
+plt.title('Daily high and low temperatures, 2022', fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperatures (F)', fontsize=16)
