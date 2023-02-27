1269afbcf1437ca557412844510a66dc2650978b
Bill Cathey <billcathey@hotmail.com>
2023-02-27 16:07:16 -0500

    Fetch data on top article on Hacker News, explore structure of response data

---
+++ hn_article.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+# Make API call, store response
+url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# Explore structure of data
+response_dict = r.json()
+readable_file = 'data/readable_hn_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(response_dict, f, indent=4)
