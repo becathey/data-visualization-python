75ba4946091c628d6a6b01c4e7aa9d1d41083e78
Bill Cathey <billcathey@hotmail.com>
2023-02-26 23:19:19 -0500

    Create script to call GitHub API, get and explore info about repos

---
+++ python_repos.py
@@ -0,0 +1,28 @@
+import requests
+
+# Make API call and store response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+
+print(f"Status code: {r.status_code}")
+
+# Store API response in variable
+response_dict = r.json()
+
+# Process results
+print(f"Total repositories: {response_dict['total_count']}")
+
+# Explore info about repos
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+print('\nSelected info about each repo:')
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
